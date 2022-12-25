819c1c0d41af305dea64eb192138c22d2eb9f705
Grigoredragos1989 <grigoredragos21@gmail.com>
2022-12-25 09:47:30 +0200

    25.12

---
+++ curs1/ap1.js
@@ -0,0 +1,7 @@
+alert( 'Salut JavaScript !')
+window.alert('Salut')
+confirm('vreau sa invat Javascript')
+console.log('Mesaj in consola')
+console.info('info status')
+console.error('mesaj de eroare')
+
